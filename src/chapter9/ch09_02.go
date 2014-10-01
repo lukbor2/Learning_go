7517bd284846d362bf2e7272d6f6ea12f4d6db4e
lukbor2 <lukbor2@gmail.com>
2014-09-30 21:46:37 -0700

    sdfsfd

--- src/chapter9/ch09_02.go
+++ src/chapter9/ch09_02.go
@@ -7,17 +7,17 @@ import ("fmt"
         "math")
 
 
-type Cacca struct {
+type Circle struct {
         x, y, r float64
     }
 
-func (c *Circle2) area() float64{
+func (c *Circle) area() float64{
     return c.r*c.r*math.Pi   
 }
 
 func main(){
     
-    c := Circle2{0,0,5}
+    c := Circle{0,0,5}
     fmt.Println("My Circle is: ", c.x, c.y, c.r)
     fmt.Println("Area of my Circle is: ", c.area())
 }
